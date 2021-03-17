7386f6401d8fc1283eb6f39cb52e7045c9c2f683
deenaannag <mca515@rajagiri.edu>
2021-03-17 12:47:42 +0530

    msg

--- resources/views/Faculty.blade.php
+++ resources/views/Faculty.blade.php
@@ -35,34 +35,43 @@
     </div>
   </div>
 </nav>
-    <div class="container">
-    <div class="row">
-    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 ">
-    <table class="table table-borderless">
-    <tr>
-        <td>username</td>
-        <td><input type="text" class="form-control"></td>
-    </tr>
-    <tr>
-        <td>Roll No</td>
-        <td><input type="text" class="form-control"></td>
-    </tr>
-    <tr>
-        <td>Admission No</td>
-        <td><input type="text" class="form-control"></td>
-    </tr>
-    <tr>
-        <td>Password</td>
-        <td><input type="password" class="form-control"></td>
-    </tr>
-    <tr>
-        <td></td>
-        <td><button class="btn btn-success">Submit</button></td>
-    </tr>
-    </table>
-    </div>
-    </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-6 col-md-6 col-lg-6">
+<table class="table table-borderless">
+<tr>
+    <td>Faculty Name</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Department Name</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Address</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Phone No</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>Education Qualification</td>
+    <td><input type="text" class="form-control"></td>
+</tr>
+<tr>
+    <td>College</td>
+    <td><input type="text" class="form-control"></td>
+
+</tr>
+<tr>
+    <td></td>
+    <td><button class="btn btn-success">Submit</button></td>
+</tr>
+</table>
+</div>
+</div>
+</div>
+    
 </body>
 </html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -37,23 +36,19 @@
 </nav>
     <div class="container">
     <div class="row">
-    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 ">
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
     <table class="table table-borderless">
     <tr>
-        <td>username</td>
+        <td>Name</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Roll No</td>
-        <td><input type="text" class="form-control"></td>
+        <td>Message</td>
+        <td><textarea name="" id="" cols="30" rows="10" class="form-control"></textarea></td>
     </tr>
     <tr>
-        <td>Admission No</td>
+        <td>Phone No</td>
         <td><input type="text" class="form-control"></td>
-    </tr>
-    <tr>
-        <td>Password</td>
-        <td><input type="password" class="form-control"></td>
     </tr>
     <tr>
         <td></td>
@@ -63,6 +58,5 @@
     </div>
     </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
